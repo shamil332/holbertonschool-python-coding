886f44f272b4c266d90c2fb5ec952c6522e40e6a
shamil332 <shamilkazimli2006@gmail.com>
2025-11-22 14:47:07 +0000

    small fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """class that stores size of a square"""
     def __init__(self, size):
+    """Initialize a new square with a private size.""" 
         self.__size = size
