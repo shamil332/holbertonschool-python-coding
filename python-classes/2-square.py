a5288808bb94e44edc6da3710edf51e24ed6ef15
shamil332 <shamilkazimli2006@gmail.com>
2025-11-22 15:23:21 +0000

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,6 +12,7 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
+
     def area(self):
         """this func finds area of the square"""
         return self.__size**2
