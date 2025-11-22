41d7d16b8eab51947c7146eec667d1bf1fac589e
shamil332 <shamilkazimli2006@gmail.com>
2025-11-22 15:39:12 +0000

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A module for the class Square"""
+
+
+class Square:
+    """a class that defines a square with a size"""
+    def __init__(self, size=0):
+        """Initialize a new Square."""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """this func finds area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """prints the square using #"""
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end='')
+            print()
